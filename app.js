97a4341ae0e23843468fd474f9bd72627535259f
msdjl <msdjl@mail.ru>
2014-12-17 21:51:32 +0300

    cors

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "wiki.returnonintelligence.com");
+	res.header("Access-Control-Allow-Origin", "https://wiki.returnonintelligence.com");
 	//res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Authorization, Content-Length, X-Requested-With');
