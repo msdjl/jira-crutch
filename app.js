2a5c685d287c51a3dcf413e54628363cb4048e23
msdjl <msdjl@mail.ru>
2015-03-25 19:20:48 +0300

    jade and stylus were deleted from main code

--- app.js
+++ app.js
@@ -46,15 +46,11 @@ app.use(session({
 	})
 }));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(compress());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
@@ -452,22 +448,14 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
 	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
+		res.status(err.status || 500).json(err);
 	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
+	res.status(err.status || 500).end(err.message);
 });
 
 module.exports = app;
